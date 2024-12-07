2e64d4a016e196caf19af379d5f126d58e989bd1
FatFejziu <fat.fejziu@gmail.com>
2024-12-07 08:30:47 -0800

    lesson30

---
+++ lesson30/example3.py
@@ -0,0 +1,12 @@
+import requests
+
+from lesson23.controller import response
+
+url="https://www.ebay.com/sch/i.html?_kw=laptop"
+
+response=requests.get(url)
+
+if response.status_code==200:
+    print(response.text)
+else:
+    print(f"Failed to retrieve content from {url}")
